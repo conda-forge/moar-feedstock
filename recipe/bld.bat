5e381bc3c866dcd9b602b263032c346452b7d5be
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-08-31 15:31:25 -0300

    Fix build command

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w -X main.versionString=%PKG_VERSION%" || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w -X main.versionString=%PKG_VERSION%" ./cmd/%PKG_NAME% || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :EOF
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.versionString=${PKG_VERSION}"
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.versionString=${PKG_VERSION}" ./cmd/${PKG_NAME}
 go-licenses save . --save_path=license-files
 mkdir -p ${PREFIX}/share/man/man1
 install -m 644 ${PKG_NAME}.1 ${PREFIX}/share/man/man1/${PKG_NAME}.1
